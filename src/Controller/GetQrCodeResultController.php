fc8ca9e9985b53226499bf62a8f59a35ef3584e9
optimuim97 <assidikouattara@gmail.com>
2024-09-14 07:57:56 +0000

    update

--- src/Controller/GetQrCodeResultController.php
+++ src/Controller/GetQrCodeResultController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\CheckIns;
+use App\Repository\CheckInsRepository;
 use App\Repository\QRCodesRepository;
 use DateTime;
 use DateTimeImmutable;
@@ -14,13 +15,27 @@ use Symfony\Component\Routing\Attribute\Route;
 class GetQrCodeResultController extends AbstractController
 {
 
-    public function __construct(private QRCodesRepository $qRCodesRepo, private EntityManagerInterface $em) {}
+    public function __construct(
+        private QRCodesRepository $qRCodesRepo,
+        private EntityManagerInterface $em,
+        private CheckInsRepository $checkInsRepo
+    ) {}
 
     #[Route('/get-qr-data/{uidn}')]
     public function getQrResult($uidn)
     {
 
         $qr = $this->qRCodesRepo->findOneBy(['uidn' => $uidn]);
+        if ($qr->isUsed()) {
+            return $this->json(
+                [
+                    'status' => 'success',
+                    'message' => "Déja utilisé"
+                ],
+                Response::HTTP_OK
+            );
+        }
+
         $currentDateTime = new DateTime();
 
         if ($currentDateTime > $qr->getExpirationDate()) {
@@ -33,18 +48,20 @@ class GetQrCodeResultController extends AbstractController
             );
         }
 
-        $checkIn = new CheckIns();
-        $checkIn->setCheckInTime(new DateTimeImmutable());
-        $checkIn->setVisitor($qr->getVisitor());
-        $checkIn->setQrCode($qr);
-        $checkIn->setCreatedAt(new DateTimeImmutable());
+        $isAlreadyCheckIn = $this->checkInsRepo->findBy(['qr_code' => $qr]);
+        if (sizeof($isAlreadyCheckIn) > 0) {
+            $this->updateCheckIn($qr);
 
-        $qr->addCheckIn($checkIn);
-        $this->em->persist($checkIn);
+            return $this->json(
+                [
+                    'status' => 'success',
+                    'message' => "CheckOut éffectué"
+                ],
+                Response::HTTP_OK
+            );
+        }
 
-        $qr->setUsed(true);
-        $this->em->persist($qr);
-        $this->em->flush();
+        $this->saveCheckIn($qr);
 
         return $this->json(
             [
@@ -53,5 +70,31 @@ class GetQrCodeResultController extends AbstractController
             ],
             Response::HTTP_OK
         );
+    }
+
+    public function saveCheckIn($qr)
+    {
+        $checkIn = new CheckIns();
+        $checkIn->setCheckInTime(new DateTimeImmutable());
+        $checkIn->setVisitor($qr->getVisitor());
+        $checkIn->setQrCode($qr);
+        $checkIn->setCreatedAt(new DateTimeImmutable());
+
+        $qr->addCheckIn($checkIn);
+        $this->em->persist($checkIn);
+        $this->em->persist($qr);
+        $this->em->flush();
+    }
+
+    public function updateCheckIn($qr)
+    {
+        $checkIn = $this->checkInsRepo->findOneBy(['qr_code' => $qr]);
+        $checkIn->setCheckOutTime(new DateTimeImmutable());
+        $qr->addCheckIn($checkIn);
+        $this->em->persist($checkIn);
+
+        $qr->setUsed(true);
+        $this->em->persist($qr);
+        $this->em->flush();
     }
 }
