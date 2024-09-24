8fffa844a394524ff8d9a5cfe9577b895f7bb035
optimuim97 <assidikouattara@gmail.com>
2024-09-24 10:31:52 +0000

    secure company

--- src/Controller/UserInfoController_.php
+++ src/Controller/UserInfoController_.php
@@ -9,7 +9,7 @@ use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\User\UserInterface;
 
-class UserInfoController extends AbstractController
+class UserInfoController_ extends AbstractController
 {
     #[Route('/api/user/info', name: 'api_user_info', methods: ['GET'])]
     public function getUserInfo(): JsonResponse
